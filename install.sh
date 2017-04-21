cf31ce09e95c55d896a35acf4417f87cdee0c74d
ThibF <thibaultferrante@hotmail.com>
2017-04-21 22:59:35 +0200

    add install in order to disable desktop in nautilus on ubuntu

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+
+python -mplatform | grep -qi Ubuntu && gsettings set org.gnome.desktop.background show-desktop-icons false
 mkdir ~/.i3
 cp configFr ~/.i3/
 cp workspaceFirmware.json ~/.i3/
 cp workspaceDashboard2.json ~/.i3/
-sudo apt-get install rxvt-unicode
+sudo apt-get install rxvt-unicode zsh
 
 
