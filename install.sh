3f839afc3804d7e8afe17367df6cef0ef5080dbb
ThibF <thibaultferrante@hotmail.com>
2019-02-19 16:29:35 +0000

    Add xsel to install package

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir ~/.i3
 ln -s configFr ~/.i3/configFr
 ln -s configUs ~/.i3/configUs
 ln -s configUs ~/.i3/config
-sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome
+sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel
 ln -s i3status.conf ~/.i3status.conf
 ln -s .Xresources ~/.Xressources
 ln -s i3blocks ~/.config/i3blocks
