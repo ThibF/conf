9b6f840f973d5718ddbadd4edd66870feb01eac2
ThibF <thibaultferrante@hotmail.com>
2019-04-03 14:26:41 +0000

    Add nano to installs

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir ~/.i3
 ln -s configFr ~/.i3/configFr
 ln -s configUs ~/.i3/configUs
 ln -s configUs ~/.i3/config
-sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel
+sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano
 ln -s i3status.conf ~/.i3status.conf
 ln -s .Xresources ~/.Xressources
 ln -s i3blocks ~/.config/i3blocks
