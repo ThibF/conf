c5baa63009ce4db0ab3820dc2013000c1a2fbe1f
ThibF <thibault.ferrante@ev-box.com>
2019-06-30 14:39:28 +0200

    Update

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 python -mplatform | grep -qi Ubuntu && gsettings set org.gnome.desktop.background show-desktop-icons false
 mkdir ~/.i3
-ln -s configFr ~/.i3/configFr
-ln -s configUs ~/.i3/configUs
-ln -s configUs ~/.i3/config
-sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano
-ln -s i3status.conf ~/.i3status.conf
-ln -s .Xresources ~/.Xressources
-ln -s i3blocks ~/.config/i3blocks
+ln -fs $(pwd)/configFr ~/.i3/configFr
+ln -fs $(pwd)/configUs ~/.i3/configUs
+ln -fs $(pwd)/configUs ~/.i3/config
+sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano i3 curl git
+ln -fs $(pwd)/i3status.conf ~/.i3status.conf
+ln -fs $(pwd)/.Xresources ~/.Xressources
+ln -fs $(pwd)/i3blocks ~/.config/i3blocks
 sudo apt-get install rxvt-unicode
 
 
