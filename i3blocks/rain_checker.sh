8c7a3fc308ec958107eb3532887f146fdf81a212
thibault.ferrante <thibault.ferrante@gmail.com>
2020-01-28 10:53:25 +0100

    Catch up

---
+++ i3blocks/rain_checker.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#Keep in mind: official values are 0.25, 1, 2.5
+LIGHT_RAIN_TRESHOLD=0.1
+MEDIUM_RAIN_TRESHOLD=0.25
+MASSIVE_RAIN_TRESHOLD=1
+
+is_bigger(){
+  echo "$1 >= $2" | bc
+}
+
+is_int() {
+    # usage: is_int "number"
+    printf %d "$1" >/dev/null 2>&1
+}
+set -e
+
+PRECIPITATIONS_VALUES=$(curl -s 'https://cdn-secure.buienalarm.nl/api/3.4/forecast.php?lat=52.3404&lon=4.8669&region=nl&unit=mm/u' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Origin: https://www.buienalarm.nl' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://www.buienalarm.nl/' | jq .precip)
+#echo $PRECIPITATIONS_VALUES
+
+_number_of_line_printed="0"
+for _value in $PRECIPITATIONS_VALUES; do
+  if ! is_int "${_value%%[,.]*}"; then
+    continue
+  fi
+
+  if [ "$(is_bigger ${_value%%[,]*} ${MASSIVE_RAIN_TRESHOLD})" -eq 1 ]; then
+    printf "⛈️"
+  elif [ "$(is_bigger ${_value%%[,]*} ${MEDIUM_RAIN_TRESHOLD})" -eq 1 ]; then
+    printf "🌧️"
+  elif [ "$(is_bigger ${_value%%[,]*} ${LIGHT_RAIN_TRESHOLD})" -eq 1 ]; then
+    printf "☁️"
+  else
+    printf "."
+  fi
+  _number_of_line_printed=$((_number_of_line_printed + 1))
+  if [ "${_number_of_line_printed}" -gt 23 ]; then
+    exit 0
+  fi
+  if [ `expr $_number_of_line_printed % 6` -eq 0 ]; then
+    if [ "${BLOCK_BUTTON}" -eq 1 ]; then
+      printf "|"
+    else
+      exit 0
+    fi
+  fi
+done
+
--- install.sh
+++ install.sh
@@ -5,10 +5,26 @@ mkdir ~/.i3
 ln -fs $(pwd)/configFr ~/.i3/configFr
 ln -fs $(pwd)/configUs ~/.i3/configUs
 ln -fs $(pwd)/configUs ~/.i3/config
-sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano i3 curl git
+sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano i3 curl git compton
 ln -fs $(pwd)/i3status.conf ~/.i3status.conf
 ln -fs $(pwd)/.Xresources ~/.Xressources
 ln -fs $(pwd)/i3blocks ~/.config/i3blocks
 sudo apt-get install rxvt-unicode
+sudo ln -fs $(pwd)/xorg.conf.new /etc/X11/xorg.conf
+#sudo apt install dbus lua-lgi lua-posix lua5.1 luajit qemu qemu-user-static htop meld pavucontrol automake screen microcom u-boot-tools device-tree-compiler iperf chromium-browser kdeconnect nmap
+#sudo apt install qemu-user-static
 
+
+# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+#  sudo apt update && sudo apt install sublime-text
+
+
+# sudo apt install autojump
+# echo ". /usr/share/autojump/autojump.sh" >> ~/.zshrc
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#sudo usermod -a -G dialout ${USER}
+#sudo usermod -a -G docker ${USER}
 
