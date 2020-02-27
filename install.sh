fb2e4d495c7609461526fe5f8e981041d2a7ac97
thibault.ferrante <thibault.ferrante@gmail.com>
2020-02-27 15:57:00 +0100

    Improve installing with escaping and more reliability

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 
 python -mplatform | grep -qi Ubuntu && gsettings set org.gnome.desktop.background show-desktop-icons false
 mkdir ~/.i3
-ln -fs $(pwd)/configFr ~/.i3/configFr
-ln -fs $(pwd)/configUs ~/.i3/configUs
-ln -fs $(pwd)/configUs ~/.i3/config
+ln -fs "$(pwd)/configFr" ~/.i3/configFr
+ln -fs "$(pwd)/configUs" ~/.i3/configUs
+ln -fs "$(pwd)/configUs" ~/.i3/config
 sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano i3 curl git compton unzip
-ln -fs $(pwd)/i3status.conf ~/.i3status.conf
-ln -fs $(pwd)/.Xresources ~/.Xressources
-ln -fs $(pwd)/i3blocks ~/.config/i3blocks
+ln -fs "$(pwd)/i3status.conf" ~/.i3status.conf
+ln -fs "$(pwd)/.Xresources" ~/.Xressources
+ln -fs "$(pwd)/i3blocks" ~/.config/i3blocks
 sudo apt-get install rxvt-unicode
-sudo ln -fs $(pwd)/xorg.conf.new /etc/X11/xorg.conf
+sudo ln -fs "$(pwd)/xorg.conf.new" /etc/X11/xorg.conf
 
 if command -v nautilus; then
-	cp $(pwd)/user-dirs.dirs .config/user-dirs.dirs
+	cp "$(pwd)/user-dirs.dirs" ~/.config/user-dirs.dirs
 fi
 
 echo "Installing JetBrainsMono-1 font"
@@ -21,8 +21,10 @@ if wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip; then
 	if unzip JetBrainsMono-1.0.3.zip; then
 		sudo mv JetBrainsMono-1.0.3 /usr/share/fonts/
 	fi
+	rm JetBrainsMono-1.0.3.zip
 	printf "\rFor subl: preferences/settings\n"
 	printf '\r\r "font_face": "JetBrains Mono Regular", \n'
+	xrdb -load ~/.Xressources
 	i3-msg "reload"
 fi
 #sudo apt install dbus lua-lgi lua-posix lua5.1 luajit qemu qemu-user-static htop meld pavucontrol automake screen microcom u-boot-tools device-tree-compiler iperf chromium-browser kdeconnect nmap
