f6c998cf08a9f8e5b0189a84428ef8026b299bf9
thibault.ferrante <thibault.ferrante@gmail.com>
2020-02-27 15:51:55 +0100

    Add jetbrain font

--- install.sh
+++ install.sh
@@ -5,12 +5,22 @@ mkdir ~/.i3
 ln -fs $(pwd)/configFr ~/.i3/configFr
 ln -fs $(pwd)/configUs ~/.i3/configUs
 ln -fs $(pwd)/configUs ~/.i3/config
-sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano i3 curl git compton
+sudo apt-get install rxvt-unicode zsh feh i3blocks fonts-font-awesome xsel nano i3 curl git compton unzip
 ln -fs $(pwd)/i3status.conf ~/.i3status.conf
 ln -fs $(pwd)/.Xresources ~/.Xressources
 ln -fs $(pwd)/i3blocks ~/.config/i3blocks
 sudo apt-get install rxvt-unicode
 sudo ln -fs $(pwd)/xorg.conf.new /etc/X11/xorg.conf
+
+echo "Installing JetBrainsMono-1 font"
+if wget https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip; then
+	if unzip JetBrainsMono-1.0.3.zip; then
+		sudo mv JetBrainsMono-1.0.3 /usr/share/fonts/
+	fi
+	printf "\rFor subl: preferences/settings\n"
+	printf '\r\r "font_face": "JetBrains Mono Regular", \n'
+	i3-msg "reload"
+fi
 #sudo apt install dbus lua-lgi lua-posix lua5.1 luajit qemu qemu-user-static htop meld pavucontrol automake screen microcom u-boot-tools device-tree-compiler iperf chromium-browser kdeconnect nmap
 #sudo apt install qemu-user-static
 
